8f50cd96ac01e18a8604ec43909d6ff63a0ef8a2
sergio-pacheco <pachecosergioe@gmail.com>
2023-09-22 15:15:44 -0300

    seguir trabajando

---
+++ practica.java
@@ -0,0 +1,15 @@
+
+ 
+public class practica{
+    public static void main(String[] args) {
+    int num1=10;
+    int num2=30;
+    int suma= num1 + num2;
+    System.out.println("la suma de num1 + num2 es:"+ suma);
+
+    }
+
+    }
+    
+        
+
